5111e5d0b1ef91d032a507c185dc5832746e512e
Ehmad Saeed <justehmadsaeed@gmail.com>
2021-03-26 19:44:53 +0500

    📦 NEW: Add Logo and Navigation Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Logo from './components/Logo/Logo'
+import Navigation from './components/Navigation/Navigation'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className='App'>
+			<Navigation />
+			<Logo />
+			{/* 
+			<ImageLinkForm />
+			<FaceRecognition /> */}
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Tilt from 'react-tilt'
+import './Logo.css'
+import brain from './brain.png'
+
+const Logo = () => {
+	return (
+		<div className='ma4 mt0'>
+			<Tilt
+				className='Tilt br2 shadow-2'
+				options={{ max: 55 }}
+				style={{ height: 100, width: 100 }}
+			>
+				<div className='Tilt-inner pa3'>
+					<img style={{ paddingTop: '3px' }} src={brain} alt='logo' />
+				</div>
+			</Tilt>
+		</div>
+	)
+}
+
+export default Logo
