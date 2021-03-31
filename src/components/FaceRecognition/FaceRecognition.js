d8e1fe950b4a1a7e78d77157288448d905bdd982
Ehmad Saeed <justehmadsaeed@gmail.com>
2021-03-31 21:32:22 +0500

    📦 NEW: Draw Bounding Box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import './FaceRecognition.css'
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ box, imageUrl }) => {
 	return (
 		<div className='center ma'>
 			<div className='absolute mt2'>
-				<img src={imageUrl} alt='' width='500px' height='auto' />
+				<img
+					id='input-image'
+					src={imageUrl}
+					alt=''
+					width='500px'
+					height='auto'
+				/>
+				<div
+					className='bounding-box'
+					style={{
+						top: box.topRow,
+						right: box.rightCol,
+						left: box.leftCol,
+						bottom: box.bottomRow
+					}}
+				></div>
 			</div>
 		</div>
 	)
