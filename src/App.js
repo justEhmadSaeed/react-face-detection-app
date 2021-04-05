2b8a39e7522998885f94c94d1748335391754e55
Ehmad Saeed <justehmadsaeed@gmail.com>
2021-04-05 10:09:25 +0500

    👌 IMPROVE: Add placeholder & a default link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react'
-import './App.css'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
-import Logo from './components/Logo/Logo'
-import Navigation from './components/Navigation/Navigation'
-import Rank from './components/Rank/Rank'
-import Particles from 'react-particles-js'
-import Clarifai from 'clarifai'
-import FaceRecognition from './components/FaceRecognition/FaceRecognition'
-import SignIn from './components/SignIn/SignIn'
-import Register from './components/Register/Register'
+import React, { useState } from 'react';
+import './App.css';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Logo from './components/Logo/Logo';
+import Navigation from './components/Navigation/Navigation';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 
 const particlesOptions = {
 	particles: {
@@ -16,56 +16,58 @@ const particlesOptions = {
 			value: 40,
 			density: {
 				enable: true,
-				value_area: 300
-			}
-		}
-	}
-}
+				value_area: 300,
+			},
+		},
+	},
+};
 
 const app = new Clarifai.App({
-	apiKey: 'cf4f0c2c945948578669896929cb81d4'
-})
+	apiKey: 'cf4f0c2c945948578669896929cb81d4',
+});
 const App = () => {
-	const [input, setInput] = useState('')
-	const [imageUrl, setImageUrl] = useState('')
-	const [box, setBox] = useState({})
-	const [route, setRoute] = useState('signin')
-	const [isSignedIn, setisSignedIn] = useState(false)
+	const [input, setInput] = useState(
+		'https://scontent.fkhi5-1.fna.fbcdn.net/v/t1.6435-9/97235140_2596161327298472_6461337611712593920_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeGmcllb2JKiXQJHHEaf8W_VXNYfmp9vyUlc1h-an2_JSR_vk95KT7Nt1HWul_BdBxgAI1ddp1tgyuItEgnPcivB&_nc_ohc=RfRdR7-mPo8AX8WJ6B2&_nc_ht=scontent.fkhi5-1.fna&oh=75c2547c49730cae7c2f5f5c03ce5115&oe=60910B48'
+	);
+	const [imageUrl, setImageUrl] = useState('');
+	const [box, setBox] = useState({});
+	const [route, setRoute] = useState('signin');
+	const [isSignedIn, setisSignedIn] = useState(false);
 
-	const onInputChange = (event) => setInput(event.target.value)
+	const onInputChange = (event) => setInput(event.target.value);
 
 	const calculateFaceLocation = (data) => {
 		const clarifaiFace =
-			data.outputs[0].data.regions[0].region_info.bounding_box
-		const image = document.getElementById('input-image')
-		const width = Number(image.width)
-		const height = Number(image.height)
+			data.outputs[0].data.regions[0].region_info.bounding_box;
+		const image = document.getElementById('input-image');
+		const width = Number(image.width);
+		const height = Number(image.height);
 
 		return {
 			leftCol: clarifaiFace.left_col * width,
 			topRow: clarifaiFace.top_row * height,
 			rightCol: width - clarifaiFace.right_col * width,
-			bottomRow: height - clarifaiFace.bottom_row * height
-		}
-	}
+			bottomRow: height - clarifaiFace.bottom_row * height,
+		};
+	};
 
 	const onButtonSubmit = () => {
-		setImageUrl(input)
+		setImageUrl(input);
 		app.models
 			.predict(Clarifai.FACE_DETECT_MODEL, input)
 			.then((response) => {
-				setBox(calculateFaceLocation(response))
+				setBox(calculateFaceLocation(response));
 			})
-			.catch((err) => console.log(err))
-	}
+			.catch((err) => console.log(err));
+	};
 	const onRouteChange = (route) => {
-		if (route === 'signout') setisSignedIn(false)
-		else if (route === 'home') setisSignedIn(true)
+		if (route === 'signout') setisSignedIn(false);
+		else if (route === 'home') setisSignedIn(true);
 
-		setRoute(route)
-	}
+		setRoute(route);
+	};
 
-	console.log(box)
+	console.log(box);
 	return (
 		<div className='App'>
 			<Particles className='particles' params={particlesOptions} />
@@ -86,7 +88,7 @@ const App = () => {
 				<Register onRouteChange={onRouteChange} />
 			)}
 		</div>
-	)
-}
+	);
+};
 
-export default App
+export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './ImageLinkForm.css'
+import React from 'react';
+import './ImageLinkForm.css';
 
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
@@ -12,7 +12,9 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 					<input
 						className='f5 pa3 w-70 center'
 						type='text'
+						defaultValue='https://scontent.fkhi5-1.fna.fbcdn.net/v/t1.6435-9/97235140_2596161327298472_6461337611712593920_n.jpg?_nc_cat=110&amp;ccb=1-3&amp;_nc_sid=09cbfe&amp;_nc_eui2=AeGmcllb2JKiXQJHHEaf8W_VXNYfmp9vyUlc1h-an2_JSR_vk95KT7Nt1HWul_BdBxgAI1ddp1tgyuItEgnPcivB&amp;_nc_ohc=RfRdR7-mPo8AX8WJ6B2&amp;_nc_ht=scontent.fkhi5-1.fna&amp;oh=75c2547c49730cae7c2f5f5c03ce5115&amp;oe=60910B48'
 						onChange={onInputChange}
+						placeholder='Enter an image URL'
 					/>
 					<button
 						onClick={onButtonSubmit}
@@ -23,7 +25,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 				</div>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default ImageLinkForm
+export default ImageLinkForm;
